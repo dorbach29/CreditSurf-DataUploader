6664532ceaadf4a80024a226fccb6649d70cdd42
DanielOrbach <dorbach29@gmail.com>
2021-01-11 18:19:19 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('xlsx');
+
+console.log("Application is running");
+
+const headerDict = {
+    CardName : "", 	
+    CardType : "",
+    CreditNetwork: "",
+    Bank: "",
+    GuideLink: "",
+    CardLink: "",
+    TimeDomestic: "",
+    TimeInternational: "",
+    CoverageLimit: "",
+    PriceLimit: "",
+    CoverageType: "",
+    CarsExcluded: "",	
+    CountriesExcluded: "",	
+    Steps: "",	
+    ClaimSteps: "",
+    ClaimDocuments : ""
+};
